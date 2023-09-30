e2497b6074241599ff3b90c573348dc421bc8d79
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-09-30 15:46:58 -0700

    compatibility with other mods that use simhelper funccapture for main menu background simulation scripts

--- data-updates.lua
+++ data-updates.lua
@@ -5,20 +5,22 @@ local on_entity_died = spill.on_entity_died
 local on_player_died = spill.on_player_died
 local on_post_entity_died = spill.on_post_entity_died
 
-local function on_init()
+local function simulation_init()
     script.on_event(defines.events.on_entity_died, on_entity_died)
     script.on_event(defines.events.on_pre_player_died, on_player_died)
     script.on_event(defines.events.on_post_entity_died, on_post_entity_died)
 end
 
-local init_script = func_capture.capture(on_init)
-
 for _, simulation in pairs(data.raw["utility-constants"]["default"].main_menu_simulations) do
     if simulation then
         if simulation.init then
-            simulation.init = simulation.init .. init_script
+            local existing_init = assert(load(simulation.init))
+            simulation.init = func_capture.capture(function()
+                existing_init()
+                simulation_init()
+            end)
         else
-            simulation.init = init_script
+            simulation.init = func_capture.capture(simulation_init)
         end
     end
 end
