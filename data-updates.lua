b636fe58a66fbedb59f74e619856e01a471e6892
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-01-06 23:35:28 -0800

    remove main menu simulations

--- data-updates.lua
+++ data-updates.lua
@@ -1,26 +1,30 @@
 
-local func_capture = require "__simhelper__.funccapture"
-local spill = require "spill"
-local on_entity_died = spill.on_entity_died
-local on_player_died = spill.on_player_died
-local on_post_entity_died = spill.on_post_entity_died
+-- local func_capture = require "__simhelper__.funccapture"
+-- local spill = require "spill"
+-- local on_entity_died = spill.on_entity_died
+-- local on_player_died = spill.on_player_died
+-- local on_post_entity_died = spill.on_post_entity_died
 
-local function simulation_init()
-    script.on_event(defines.events.on_entity_died, on_entity_died)
-    script.on_event(defines.events.on_pre_player_died, on_player_died)
-    script.on_event(defines.events.on_post_entity_died, on_post_entity_died)
-end
+-- local function simulation_init()
+--     script.on_event(defines.events.on_entity_died, on_entity_died)
+--     script.on_event(defines.events.on_pre_player_died, on_player_died)
+--     script.on_event(defines.events.on_post_entity_died, on_post_entity_died)
+-- end
 
-for _, simulation in pairs(data.raw["utility-constants"]["default"].main_menu_simulations) do
-    if simulation then
-        if simulation.init then
-            local existing_init = assert(load(simulation.init))
-            simulation.init = func_capture.capture(function()
-                existing_init()
-                simulation_init()
-            end)
-        else
-            simulation.init = func_capture.capture(simulation_init)
-        end
-    end
-end
+-- local existing_on_entity_died = script.get_event_handler(defines.events.on_entity_died)
+
+-- for _, simulation in pairs(data.raw["utility-constants"]["default"].main_menu_simulations) do
+--     if simulation then
+--         if simulation.init then
+--             local existing_init = assert(load(simulation.init))
+--             simulation.init = func_capture.capture(function()
+--                 existing_init()
+--                 simulation_init()
+--             end)
+--         else
+--             simulation.init = func_capture.capture(function()
+--                 simulation_init()
+--             end)
+--         end
+--     end
+-- end
