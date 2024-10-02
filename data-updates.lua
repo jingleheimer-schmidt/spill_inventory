1fbecdf3452d57728d6fd0fe9b273b4cad009ab8
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-10-01 19:14:13 -0700

    Update data-updates.lua

--- data-updates.lua
+++ data-updates.lua
@@ -4,7 +4,9 @@ for _, simulation in pairs(data.raw["utility-constants"]["default"].main_menu_si
         if simulation.mods then
             table.insert(simulation.mods, "spill_inventory")
         else
-            simulation.mods = { "spill_inventory" }
+            simulation.mods = {
+                [1] = "spill_inventory",
+            }
         end
     end
 end
