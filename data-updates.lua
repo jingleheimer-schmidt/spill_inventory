1d1549f99655c4f33850d6d26d21e122132d13af
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-10-01 18:38:32 -0700

    re-added main menu simulation integration

--- data-updates.lua
+++ data-updates.lua
@@ -1,30 +1,10 @@
 
--- local func_capture = require "__simhelper__.funccapture"
--- local spill = require "spill"
--- local on_entity_died = spill.on_entity_died
--- local on_player_died = spill.on_player_died
--- local on_post_entity_died = spill.on_post_entity_died
-
--- local function simulation_init()
---     script.on_event(defines.events.on_entity_died, on_entity_died)
---     script.on_event(defines.events.on_pre_player_died, on_player_died)
---     script.on_event(defines.events.on_post_entity_died, on_post_entity_died)
--- end
-
--- local existing_on_entity_died = script.get_event_handler(defines.events.on_entity_died)
-
--- for _, simulation in pairs(data.raw["utility-constants"]["default"].main_menu_simulations) do
---     if simulation then
---         if simulation.init then
---             local existing_init = assert(load(simulation.init))
---             simulation.init = func_capture.capture(function()
---                 existing_init()
---                 simulation_init()
---             end)
---         else
---             simulation.init = func_capture.capture(function()
---                 simulation_init()
---             end)
---         end
---     end
--- end
+for _, simulation in pairs(data.raw["utility-constants"]["default"].main_menu_simulations) do
+    if simulation then
+        if simulation.mods then
+            table.insert(simulation.mods, "spill_inventory")
+        else
+            simulation.mods = { "spill_inventory" }
+        end
+    end
+end
