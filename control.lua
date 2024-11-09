7d94294d86c4e35746382d8996328872ce2d7eb7
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-11-08 16:25:26 -0800

    Update control.lua

--- control.lua
+++ control.lua
@@ -2,6 +2,8 @@
 local spill = require "spill"
 local on_entity_died = spill.on_entity_died
 local on_player_died = spill.on_player_died
+local on_post_entity_died = spill.on_post_entity_died
 
 script.on_event(defines.events.on_entity_died, on_entity_died)
 script.on_event(defines.events.on_pre_player_died, on_player_died)
+script.on_event(defines.events.on_post_entity_died, on_post_entity_died)
